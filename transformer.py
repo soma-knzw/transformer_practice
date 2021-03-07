400bc6e40a3358b7b35f4c38a61ade4d61627cf8
soma-knzw <kanazawa@ailab.ics.keio.ac.jp>
2021-03-07 07:50:33 +0000

    ADD: Transformer

---
+++ transformer.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+
+from encoder import Encoder
+from decoder import Decoder
+
+
+class Transformer(nn.Module):
+    def __init__(
+            self,
+            src_vocab_size,
+            trg_vocab_size,
+            src_pad_idx,
+            trg_pad_idx,
+            device='cpu',
+            embed_size=256,
+            num_layers=6,
+            forward_expansion=4,
+            heads=8,
+            dropout=0,
+            max_length=100):
+        super(Transformer, self).__init__()
+
+        self.encoder = Encoder(
+            src_vocab_size,
+            embed_size,
+            num_layers,
+            heads,
+            device,
+            forward_expansion,
+            dropout,
+            max_length)
+
+        self.decoder = Decoder(
+            trg_vocab_size,
+            embed_size,
+            num_layers,
+            heads,
+            forward_expansion,
+            dropout,
+            device,
+            max_length)
+
+        self.src_pad_idx = src_pad_idx
+        self.trg_pad_idx = trg_pad_idx
+        self.device = device
+
+    def make_src_mask(self, src):
+        # (N, 1, 1, src_len)
+        src_mask = (src != self.src_pad_idx).unsqueeze(1).unsqueeze(2)
+
+        return src_mask.to(self.device)
+
+    def make_trg_mask(self, trg):
+        N, trg_len = trg.shape
+
+        tmp = torch.oens(trg_len, trg_len)
+        trg_mask = torch.tril(tmp).expand(N, 1, trg_len, trg_len)
+
+        return trg_mask.to(self.device)
+
+    def forward(self, src, trg):
+        src_mask = self.make_src_mask(src)
+        trg_mask = self.make_trg_mask(trg)
+
+        enc_src = self.encoder(src, src_mask)
+
+        out = self.decoder(trg, enc_src, src_mask, trg_mask)
+
+        return out
+
+
+if __name__ == '__main__':
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # tmp
+    device = 'cpu'
+
+    # dummy sequence
+    # tokens
+    # 1: start, 2: end, 0: padding
+    x = torch.tensor([[1, 5, 6, 4, 3, 9, 5, 2, 0],
+                      [1, 8, 7, 3, 4, 5, 6, 7, 2]]).to(device)
+    trg = torch.tensor([[1, 7, 4, 3, 5, 9, 2, 0],
+                        [1, 5, 6, 2, 4, 7, 6, 2]]).to(device)
+
+    src_pad_idx = 0
+    trg_pad_idx = 0
+    src_vocab_size = 10
+    trg_vocab_size = 10
+
+    model = Transformer(
+        src_vocab_size,
+        trg_vocab_size,
+        src_pad_idx,
+        trg_pad_idx,
+        device=device)
+
+    out = model(x, trg[:, :-1])
+
+    print(out)
+    print(out.shape)
